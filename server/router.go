3a573e67ef914cb8d9dc2097c1e27c8a93ff6eac
Felipe Hernandez <deffeater@Felipes-MacBook-Pro.local>
2024-04-22 12:38:44 -0500

    Added admin routes

--- server/router.go
+++ server/router.go
@@ -25,13 +25,13 @@ func (s *Server) router() http.Handler {
 	// Admin Router
 	adminRouter := router.PathPrefix("/admin").Subrouter()
 	// adminRouter.Use(s.AuthMiddleware)
-	adminRouter.HandleFunc("/user/", s.HandleUser).Methods(http.MethodPost, http.MethodPatch)
-	// adminRouter.HandleFunc("/user/roles", s.HandleUserRoles).Methods(http.MethodGet, http.MethodPost, http.MethodPatch)
-	adminRouter.HandleFunc("/role/", s.HandleRole).Methods(http.MethodPost, http.MethodPatch)
-	// adminRouter.HandleFunc("/role/permissions", s.HandleRolePermissions).Methods(http.MethodGet, http.MethodPost, http.MethodPatch)
-	adminRouter.HandleFunc("/permission/", s.HandlePermission).Methods(http.MethodPost, http.MethodGet)
-	// adminRouter.HandleFunc("/client/", s.HandleClient).Methods(http.MethodPost, http.MethodPatch)
-	// adminRouter.HandleFunc("/application/", s.HandleApplication).Methods(http.MethodPost, http.MethodPatch)
+	adminRouter.HandleFunc("/user/", s.HandleUser).Methods(http.MethodGet, http.MethodPost)
+	adminRouter.HandleFunc("/user/roles", s.HandleUserRoles).Methods(http.MethodGet, http.MethodPost, http.MethodPatch)
+	adminRouter.HandleFunc("/role/", s.HandleRole).Methods(http.MethodGet, http.MethodPost)
+	adminRouter.HandleFunc("/role/permissions", s.HandleRolePermissions).Methods(http.MethodGet, http.MethodPost, http.MethodPatch)
+	adminRouter.HandleFunc("/permission/", s.HandlePermission).Methods(http.MethodGet, http.MethodPost)
+	adminRouter.HandleFunc("/client/", s.HandleClient).Methods(http.MethodPost, http.MethodPatch)
+	adminRouter.HandleFunc("/application/", s.HandleApplication).Methods(http.MethodPost, http.MethodPatch)
 
 	// Public Router
 	publicRouter := router.PathPrefix("/public").Subrouter()
